d58514933121a81583826130243ae03c82ae5823
younuskhan1 <md.younuskhan412@gmail.com>
2023-12-04 12:35:03 +0600

    card name is added

--- index.js
+++ index.js
@@ -15,14 +15,17 @@ const displayAiData = (aiData) => {
     aiData.forEach(data => {
         console.log(data);
         const cardDiv = document.createElement("div");
-        cardDiv.classList = `"w-[30%]"`
+        cardDiv.classList = `p-4 border border-[rgba(17, 17, 17, 0.10)] rounded-[12px]`;
         cardDiv.innerHTML = `
-        <div class="h-[250px] border border-red-600 p-3"><img class = "w-[100%] h-[100%] rounded-xl" src="${data.image}" alt=""></div>
-        <div class="pl-4 mt-2">
+        <div class="h-[250px]"><img class = "w-[100%] h-[100%] rounded-xl" src="${data.image}" alt=""></div>
+        <div class="pl-4 mt-4 border-b-2 pb-4">
             <h3 class="font-bold text-lg text-[#111]">Features :</h3>
             <p class="text-[#585858] pl-1"> 1. ${data.features[0]}</p>
             <p class="text-[#585858] pl-1"> 2. ${data.features[1]}</p>
             <p class="text-[#585858] pl-1"> 3. ${data.features[2]}</p>
+        </div>
+        <div>
+         <h2>${data.name}</h2>
         </div>
         
         `;
