b6b5ddee84315382abcee47a3a86d2d54f8b41dd
younuskhan1 <md.younuskhan412@gmail.com>
2023-12-04 10:11:29 +0600

    ai data is loaded

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("amar sonar bangla. ami tomai valobashi.")
+const loadAiData = async () => {
+    const url = `https://openapi.programming-hero.com/api/ai/tools`;
+    try {
+        const response = await fetch(url);
+        const aiTools = await response.json();
+        const aiData = aiTools.data.tools;
+        displayAiData(aiData);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+const displayAiData = (aiData) => {
+    console.log(aiData)
+}
+
+
+loadAiData();
