8bd6809787b4ac46c0f8668a02911ca8845372e4
younuskhan1 <md.younuskhan412@gmail.com>
2023-12-04 15:29:02 +0600

    see more button's work is done

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-const loadAiData = async () => {
+const loadAiData = async (dataLimit) => {
     const url = `https://openapi.programming-hero.com/api/ai/tools`;
     try {
         const response = await fetch(url);
         const aiTools = await response.json();
         const aiData = aiTools.data.tools;
-        displayAiData(aiData);
+        displayAiData(aiData, dataLimit);
     }
     catch (error) {
         console.log(error);
     }
 }
-const displayAiData = (aiData) => {
+
+const seeMoreContainer = document.getElementById("see-more-container");
+
+const displayAiData = (aiData, dataLimit) => {
+    console.log(dataLimit);
     const cardContainer = document.getElementById("card-container");
+    cardContainer.innerHTML = "";
+    if (dataLimit && aiData.length > 6) {
+        aiData = aiData.slice(0, 6)
+        seeMoreContainer.classList.remove("hidden");
+    } else {
+        seeMoreContainer.classList.add("hidden");
+    }
     aiData.forEach(data => {
-        console.log(data);
+        // console.log(data);
         const cardDiv = document.createElement("div");
         cardDiv.classList = `p-4 border border-[rgba(17, 17, 17, 0.10)] rounded-[12px]`;
         cardDiv.innerHTML = `
@@ -25,8 +36,8 @@ const displayAiData = (aiData) => {
             <p class="text-[#585858] pl-1"> 3. ${data.features[2]}</p>
         </div>
         <div class="flex justify-between items-center pl-4 p-4">
-          <div>
-            <h2 class="font-bold text-lg text-[#111]">${data.name}</h2>
+          <div class="text-[13px]">
+            <h2 class="font-bold text-lg text-[#111] pb-2">${data.name}</h2>
             <p class="text-[#585858]"><i class="fa-regular fa-calendar-days pr-2"></i>${data.published_in}</P>
           </div>
           <div class="flex justify-center items-center bg-[#FEF7F7] cursor-pointer hover:bg-[#f3a5a5] rounded-full"><i class="fa-solid fa-arrow-right text-[#EB5757] py-[14px] px-[14px] hover:text-white"></i></div>
@@ -36,5 +47,8 @@ const displayAiData = (aiData) => {
     })
 }
 
+const seeMore = (dataLimit) => {
+    loadAiData(dataLimit);
+}
 
-loadAiData();
+loadAiData(true);
